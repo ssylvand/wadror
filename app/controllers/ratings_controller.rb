71fb52700b44c81329119d674420f12896177531
Samu Sylvander <samu.sylvander@saunalahti.fi>
2015-02-01 23:50:19 +0200

    Fix for ratings

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -15,6 +15,7 @@ class RatingsController < ApplicationController
 
   def create
     @rating = Rating.create params.require(:rating).permit(:score, :beer_id)
+    @rating.user = current_user
     if @rating.save
       current_user.ratings << @rating
       redirect_to user_path current_user
